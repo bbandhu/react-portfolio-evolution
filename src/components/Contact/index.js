b1a47db44499b3416e0e347d9c03148673cb5915
bbandhu <bandhavi.b12@gmail.com>
2023-07-10 00:12:51 -0700

    contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,31 +1,48 @@
 import { faHelicopterSymbol } from '@fortawesome/free-solid-svg-icons';
 import './index.scss';
 import { Link } from 'react-router-dom';
-import Loader from 'react-loaders'
+import Loader from 'react-loaders';
+import { Button, TextField } from '@material-ui/core';
 
-
-const Contact = () => 
-{
+const Contact = () => {
   return (
     <>
-    <div className="container contact-page">
-      <div className="text-zone">
-        <h1>
-          Hi, <br/> <br /> I' m B {/* <img src={LogoTitle} alt="developer" /> */}
-          Bandhavi
-          <br />
-          <br />
-            web developer
-        </h1>
-        <h2>Full stack developer / SDET </h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>Contact me</h1>
+          <p>
+            I am currently looking for a full-time software engineering role. If you have any questions or would like to connect, please feel free to reach out to me via email or LinkedIn.
+          </p>
+          <div className="contact-form">
+            <form>
+              <ul>
+                <li className="form-row">
+                  <label htmlFor="name">Name</label>
+                  <TextField id="name" type='text'variant="outlined" fullWidth InputProps={{ className: "input" }} />
+                </li>
+                <li className="form-row">
+                  <label htmlFor="email">Email</label>
+                  <TextField id="email" type="email" variant="outlined" fullWidth InputProps={{ className: "input" }} />
+                </li>
+                <li className="form-row">
+                  <label htmlFor="subject">Subject</label>
+                  <TextField id="subject" type='text' variant="outlined" fullWidth InputProps={{ className: "input" }} />
+                </li>
+                <li className="form-row">
+                  <label htmlFor="message">Message</label>
+                  <TextField id="message" type='text' multiline rows={4} variant="outlined" fullWidth InputProps={{ className: "input" }} />
+                </li>
+              </ul>
+              <Button variant="contained" color="primary" type="submit">
+                Submit
+              </Button>
+            </form>
+          </div>
+        </div>
       </div>
-    </div>
-    <Loader type="pacman" />
+      <Loader type="pacman" />
     </>
   );
-}
+};
 
 export default Contact;
