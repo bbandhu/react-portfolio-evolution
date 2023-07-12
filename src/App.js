5f13e70a74cf162d9b664f086f08d67792ffa11e
bbandhu <bandhavi.b12@gmail.com>
2023-07-11 17:44:32 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
-// Importing required dependencies and components
-import './App.scss'; // Importing the CSS styles for the App component
-import { Routes, Route } from 'react-router-dom'; // Importing routing components from react-router-dom
-import Layout from './components/Layout'; // Importing the Layout component
-import Home from './components/Home'; // Importing the Home component
-import About from './components/About'; // Importing the About component
-import Contact from './components/Contact'; // Importing the Contact component
-import Portfolio from './components/Portfolio'; // Importing the Portfolio component
-import Resume from './components/Resume'; // Importing the resume component
+import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
 
-// Defining the App component
 function App() {
   return (
-    // Routes component from react-router-dom that holds all possible routes for the app
+    <>
     <Routes>
-      {/* // A Route for the base path "/" with the Layout component as its child */}
       <Route path="/" element={<Layout />}>
-        {/* // Nested routes for different paths under "/" */}
-        {/* // The Home component will be shown when path is "/" */}
         <Route index element={<Home />} />
-        {/* // The About component will be shown when path is "/about" */}
         <Route path="about" element={<About />} />
-        {/* // The Contact component will be shown when path is "/contact" */}
         <Route path="contact" element={<Contact />} />
-        {/* // The Portfolio component will be shown when path is "/portfolio" */}
         <Route path="portfolio" element={<Portfolio />} />
-        {/* // The resume component will be shown when path is "/resume" */}
         <Route path="resume" element={<Resume />} />
       </Route>
     </Routes>
+    </>
   );
 }
 
-// Exporting the App component for use in other parts of the application
 export default App;
+
